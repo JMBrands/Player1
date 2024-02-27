a35b8c110b98602cfeed903d273e6891e0dca66b
JMBrands <jonar@ultramagic.net>
2024-02-27 13:22:31 +0100

    changed request functions

--- client/main.js
+++ client/main.js
@@ -4,16 +4,32 @@ function setPosition(id, x, y) {
     element.style.top = x + "px";
 }
 
-function sendDelta(url, action) {
-    let delta = {type: "delta", action: move};
-    const req = new Request(url);
-    const init = {
-        method: "POST",
+async function getState(gameId) {
+    const response = await fetch('http://localhost:8000/state/'+ gameId, {
+        method: 'GET',
         headers: {
-            "Content-Type": "application/json"
-        },
-        body: delta,
-    }
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+        }
+    });
 
+    const responseBody = await response.json();
 
+    return responseBody;
 }
+
+async function postDelta(gameId, playerId, delta) {
+    const response = await fetch('http://localhost:8000/delta/'+ gameId + '/' + playerId, {
+        method: 'POST',
+        headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(delta),
+    });
+
+    const responseBody = await response.json();
+
+    return responseBody;
+}
+

37db395f4ce9d369db6b432e2ee205f2a73fb96e
JMBrands <jonar@ultramagic.net>
2024-02-27 12:16:24 +0100

    styles, moving & start delta sending

--- client/main.js
+++ client/main.js
@@ -1,1 +1,19 @@
-console.log("I work!");
+function setPosition(id, x, y) {
+    let element = document.getElementById(id);
+    element.style.left = y + "px";
+    element.style.top = x + "px";
+}
+
+function sendDelta(url, action) {
+    let delta = {type: "delta", action: move};
+    const req = new Request(url);
+    const init = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: delta,
+    }
+
+
+}

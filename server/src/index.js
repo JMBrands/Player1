850e22190c53c7167f58ab059dc67d967b4ca852
Henk Oordt <hd@oordt.dev>
2024-02-27 11:15:42 +0100

    Configure express and nodemon for server

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,18 @@
-console.log("Hoi!");
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send(JSON.stringify({
+        hello: "World",
+    }));
+});
+
+app.get('/yoyo', (req, res) => {
+    my_response = JSON.parse('{"yoyo": 16}');
+    console.log(my_response);
+    res.send('yo dawg' + my_response);
+})
+
+app.listen(8000, () => {
+    console.log("Running!");
+});
